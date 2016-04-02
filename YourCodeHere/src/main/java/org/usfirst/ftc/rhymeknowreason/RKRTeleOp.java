f1de88c0fd492be297ed542f5894ff3569185248
grassclipper <dragonstad@gmail.com>
2016-04-02 15:01:22 -0400

    took out the setposition during teleop initialization
    isn't that a penalty?

--- YourCodeHere/src/main/java/org/usfirst/ftc/rhymeknowreason/RKRTeleOp.java
+++ YourCodeHere/src/main/java/org/usfirst/ftc/rhymeknowreason/RKRTeleOp.java
@@ -77,7 +77,6 @@ public class RKRTeleOp extends OpMode {
         servoFlame.setPosition(0.5);
         climberReleaser.setPosition(RKRAuto.CLIMBER_RELEASER_CLOSED);
         isClimberReleaserOpen = false;
-        plow.setPosition(0.0);
 
         mGyro.calibrate();
 
