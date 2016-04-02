0b52bf859b523ffeadd11f605a6f6494d071b8d9
DrSuess2 <truccmom@yahoo.com>
2016-04-02 13:30:43 -0400

    fixed two sources of crashes in Gyro Test

--- YourCodeHere/src/main/java/org/usfirst/ftc/rhymeknowreason/GyroTest.java
+++ YourCodeHere/src/main/java/org/usfirst/ftc/rhymeknowreason/GyroTest.java
@@ -13,34 +13,34 @@ import org.swerverobotics.library.interfaces.Disabled;
  * Created by RobotK on 1/30/2016.
  */
 @Autonomous(name = "Gyro Test")
-@Disabled
 public class GyroTest extends BaseOpMode {
-
-    TelemetryDashboardAndLog enhancedTelemetry = new TelemetryDashboardAndLog();
 
     final String LOG_TAG = "gyro-log";
 
     @Override
     public void main() throws InterruptedException {
 
+        initialize(false);
+
         gyroSensor.calibrate();
-        //enhancedTelemetry.log.add("Calibration began. Calibration status: " + gyroSensor.isCalibrating());
+        //telemetry.log.add("Calibration began. Calibration status: " + gyroSensor.isCalibrating());
         Log.d(LOG_TAG, "Calibration began. Calibration status: " + gyroSensor.isCalibrating());
         while(gyroSensor.isCalibrating()){
             Thread.sleep(50);
         }
 
-        enhancedTelemetry.log.add("Calibration finished.");
+        telemetry.log.add("Calibration finished.");
 
        // for(int i = 0; i < 300; i++) {
-            enhancedTelemetry.log.add(Double.toString(gyroSensor.getIntegratedZValue()));
+            telemetry.log.add(Double.toString(gyroSensor.getIntegratedZValue()));
          //   telemetry.addData("Gyro heading", gyroSensor.getIntegratedZValue());
          //   Thread.sleep(10);
         //}
 
         waitForStart();
-        gyroUtility.turn(360);
-        gyroUtility.turn(-360);
+        gyroUtility.turn(90);
+        Thread.sleep(1000);
+        gyroUtility.turn(-90);
     }
 }
 
